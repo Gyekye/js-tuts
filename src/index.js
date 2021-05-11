516ee1ab3f2e6ea1e4d5b1198155bbd8331c8b6f
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-05-11 15:28:43 +0000

    functions

--- src/index.js
+++ src/index.js
@@ -7,3 +7,24 @@ function squareRootCalculator(x) {
 }
 const result = squareRootCalculator(2);
 console.log(result);
+
+//Scope
+let x = 10;
+if (true) {
+  let y = 20;
+  // z is globally scoped due to the var;
+  var z = 30;
+  console.log(x + y + z);
+}
+
+console.log(x + z);
+
+// using arrow  to declare functions
+const age = (dob) => {
+  let currentYear = 2021;
+  return currentYear - dob;
+};
+console.log(`I will be ${age(2001)} this year`);
+
+let arrow = () => "single line function";
+console.log(arrow());
